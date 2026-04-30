8eaa66311bc3759e20cc0ce893bb957301c8e18b
arshadfaraz-eth <arshad.faraz@ethara.ai>
2026-04-30 13:15:08 +0530

    add requirement.txt

--- mlapi/main.py
+++ mlapi/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import pandas as pd
+import uvicorn
 
 # -----------------------------------------------------------
 # Load model & scaler
